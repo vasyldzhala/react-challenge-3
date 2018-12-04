1ef538e1301e49dcc3d48d133ba293cfd1ab95d3
Vladimir Shevchuk <dosandkv@gmail.com>
2018-12-04 19:19:18 +0200

    Update fetchRequest.js

--- src/actions/fetchRequest.js
+++ src/actions/fetchRequest.js
@@ -1,20 +1,15 @@
-const receiveData = data => dispatch => {
-  dispatch({
-    type: 'FETCH_DATA',
-    payload: data
-  })
-};
+const fetchRequest = urlArr => (dispatch, foo) => {
+  const requests = urlArr.map( url => fetch(url));
 
-const fetchRequest = (urlArr) => dispatch => {
-  dispatch(
-    (dispatch) => {
-      const requests = urlArr.map( url => fetch(url));
-      return Promise.all(requests)
-        .then(resp => Promise.all(resp.map(item => item.json())))
-        .then(resp => resp.filter(item => item.Response !== "False"))
-        .then(resp => dispatch(receiveData(resp)));
-    }
-  );
+  return Promise.all(requests)
+    .then(resp => Promise.all(resp.map(item => item.json())))
+    .then(resp => resp.filter(item => item.Response !== "False"))
+    .then(resp => {
+      dispatch({
+        type: 'FETCH_DATA',
+        payload: resp
+      })
+    })
 };
 
 export default fetchRequest;
